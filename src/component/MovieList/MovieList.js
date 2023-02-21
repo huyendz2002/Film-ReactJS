225e660715a8d7e72adf101bb042117012d880ea
huyendz2002 <122795054+huyendz2002@users.noreply.github.com>
2023-02-21 11:41:18 +0700

    Update MovieList.js

--- src/component/MovieList/MovieList.js
+++ src/component/MovieList/MovieList.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { Fragment, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import useDebounce from "../../hooks/useDebounce";
 import ReactPaginate from "react-paginate";
 import { Link } from "react-router-dom";
